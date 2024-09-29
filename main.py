e5ff81d0823cddc0d1eef571c36616b42906d218
Raffy Seyhan <raffyseyhan889@gmail.com>
2024-09-29 15:32:26 +0800

    main.py

--- main.py
+++ main.py
@@ -7,43 +7,39 @@ akun = input("Masukan Nama: ")
 nim = input("Masukan NIM: ")
 print("Akun Berhasil di Registrasi")
 
+while True:
+    print("\nLogin Akun")
+    akun_2 = input("Masukan Nama: ")
+    nim_2 = input("Masukan NIM: ")
 
-print("\nLogin Akun")
-akun_2 = input("Masukan Nama: ")
-nim_2 = input("Masukan NIM: ")
+    if akun_2 != akun or nim_2 != nim:
+        print("\nAkun Anda Salah, Silahkan Login Kembali")
+    else:
+        print("Login Berhasil")
+        break  
 
-if akun_2 != akun or nim_2 != nim:
-	print("\nAkun Anda Salah, Silahkan Login Kembali")
-	akun_2 = input("Masukan Nama: ")
-	nim_2 = input("Masukan NIM: ")
-
-if akun_2 == akun and nim_2 == nim:
-	print("Login Berhasil")
-	while True:
-		Tarif = int(input("\nSilahkan Masukan Tarif Kerja Rp/Jam: "))
-		Jam = int(input("Masukan Jam Kerja: "))
-		Gaji = Tarif * Jam
-		Bonus = Gaji * 0.1
-		Total = Bonus + Gaji
-		print ("\n======================================================================")
-		print ("Tarif kerja anda: RP.", Tarif, "/ Jam")
-		print ("Jam Kerja Anda: ", Jam, "Jam")
-		print ("Gaji Anda: RP. ", Gaji)
-		if Jam > 160:
-			print ("Bonus Anda Sebesar: RP.", Bonus)
-			print ("Total Gaji Anda Sebesar: RP.", Total)
-		print ("=======================================================================")
-		perulangan = ("\nApakah Anda Ingin Menghitung Ulang Gaji", "1. Hitung Ulang", "2. Keluar Program")
-		for y in perulangan:
-			print (y) 
-		opsi = input("Masukan Pilihan: ")
-		if opsi == "1":
-				continue
-		elif opsi == "2":
-				print("\nSampai Jumpa lagi")
-				break
-		else:
-			print("Input Yang Anda Masukan Salah Silahkan Coba Lagi")
-
-else:
-	print("Command Tidak Valid")
+while True:
+	Tarif = int(input("\nSilahkan Masukan Tarif Kerja Rp/Jam: "))
+	Jam = int(input("Masukan Jam Kerja: "))
+	Gaji = Tarif * Jam
+	Bonus = Gaji * 0.1
+	Total = Bonus + Gaji
+	print ("\n======================================================================")
+	print ("Tarif kerja anda: RP.", Tarif, "/ Jam")
+	print ("Jam Kerja Anda: ", Jam, "Jam")
+	print ("Gaji Anda: RP. ", Gaji)
+	if Jam > 160:
+		print ("Bonus Anda Sebesar: RP.", Bonus)
+		print ("Total Gaji Anda Sebesar: RP.", Total)
+	print ("=======================================================================")
+	perulangan = ("\nApakah Anda Ingin Menghitung Ulang Gaji", "1. Hitung Ulang", "2. Keluar Program")
+	for y in perulangan:
+		print (y) 
+	opsi = input("Masukan Pilihan: ")
+	if opsi == "1":
+			continue
+	elif opsi == "2":
+			print("\nSampai Jumpa lagi")
+			break
+	else:
+		print("Input Yang Anda Masukan Salah Silahkan Coba Lagi")
